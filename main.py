16724ef305463b5ac89016611d3f8f1f04f82d27
ubuntu <piotr.perz@wp.pl>
2024-10-14 21:16:24 +0200

    Day 49: submit data

--- main.py
+++ main.py
@@ -26,6 +26,26 @@ password_field.send_keys("PythonStudent123$")
 driver.execute_script("arguments[0].click();", login_button)
 # login_button.click()
 
+# Locate the Elements dropdown and Text Box
+elements = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app"]/div/div/div/div[1]/div/div/div[1]/span/div/div[1]')))
+elements.click()
+
+text_box = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'item-0')))
+text_box.click()
+
+# Locate the form fields and submit button
+fullname_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'userName')))
+email_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'userEmail')))
+current_address_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'currentAddress')))
+permanent_address = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'permanentAddress')))
+submit_button = driver.find_element(By.ID, "submit")
+
+# Fill in the form fields
+fullname_field.send_keys("Piotr Pe")
+email_field.send_keys("perzak102@wp.pl")
+current_address_field.send_keys("Zlota 22, Warszawa 1944")
+permanent_address.send_keys("Zlota 22, Warszawa 1944")
+driver.execute_script("arguments[0].click();", submit_button)
 
 input("Please Enter to close he browser")
 driver.quit()
