83dd8cc403d489d1c37a074543d6f528d56ca7b3
ubuntu <piotr.perz@wp.pl>
2024-10-14 23:15:02 +0200

    Day 49: Refactoring to OOP

--- main.py
+++ main.py
@@ -4,48 +4,88 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import os
 
-# Define driver, option, and services
-chrome_options = Options()
-chrome_options.add_argument("--disable-search-engine-choice-screen")
-service = Service('chromedriver-linux64/chromedriver')
-driver = webdriver.Chrome(options=chrome_options, service=service)
+class WebAutomation:
+    def __init__(self):
+        # Define driver, option, and services
+        chrome_options = Options()
+        chrome_options.add_argument("--disable-search-engine-choice-screen")
 
-# Load the webpage
-driver.get("https://demoqa.com/login")
+        download_path = os.getcwd()
+        prefs = {"download.default_directory": download_path}
+        chrome_options.add_experimental_option("prefs", prefs)
 
-# Locate username, password and login button
-username_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'userName')))
-password_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'password')))
-login_button = driver.find_element(By.ID, "login")
-# login_button = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'login')))
+        service = Service('chromedriver-linux64/chromedriver')
+        self.driver = webdriver.Chrome(options=chrome_options, service=service)
 
-# Fill in username and password, and click the button
-username_field.send_keys("perzak102")
-password_field.send_keys("PythonStudent123$")
-driver.execute_script("arguments[0].click();", login_button)
-# login_button.click()
+    def login(self, username, password):
+        # Load the webpage
+        self.driver.get("https://demoqa.com/login")
 
-# Locate the Elements dropdown and Text Box
-elements = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app"]/div/div/div/div[1]/div/div/div[1]/span/div/div[1]')))
-elements.click()
+        # Locate username, password and login button
+        username_field = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.ID, 'userName')))
+        password_field = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.ID, 'password')))
+        login_button = self.driver.find_element(By.ID, "login")
+        # login_button = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'login')))
 
-text_box = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'item-0')))
-text_box.click()
+        # Fill in username and password, and click the button
+        username_field.send_keys(username)
+        password_field.send_keys(password)
+        self.driver.execute_script("arguments[0].click();", login_button)
 
-# Locate the form fields and submit button
-fullname_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'userName')))
-email_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'userEmail')))
-current_address_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'currentAddress')))
-permanent_address = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'permanentAddress')))
-submit_button = driver.find_element(By.ID, "submit")
+        # login_button.click()
 
-# Fill in the form fields
-fullname_field.send_keys("Piotr Pe")
-email_field.send_keys("perzak102@wp.pl")
-current_address_field.send_keys("Zlota 22, Warszawa 1944")
-permanent_address.send_keys("Zlota 22, Warszawa 1944")
-driver.execute_script("arguments[0].click();", submit_button)
+    def fill_form(self, fullname, email, current_address, permanent_address):
+        pass
+        # Locate the Elements dropdown and Text Box
+        elements = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(
+            (By.XPATH, '//*[@id="app"]/div/div/div/div[1]/div/div/div[1]/span/div/div[1]')))
+        elements.click()
 
-input("Please Enter to close he browser")
-driver.quit()
+        text_box = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.ID, 'item-0')))
+        text_box.click()
+
+        # Locate the form fields and submit button
+        fullname_field = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.ID, 'userName')))
+        email_field = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.ID, 'userEmail')))
+        current_address_field = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.ID, 'currentAddress')))
+        permanent_address_field = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.ID, 'permanentAddress')))
+        submit_button = self.driver.find_element(By.ID, "submit")
+
+        # Fill in the form fields
+        fullname_field.send_keys(fullname)
+        email_field.send_keys(email)
+        current_address_field.send_keys(current_address)
+        permanent_address_field.send_keys(permanent_address)
+        self.driver.execute_script("arguments[0].click();", submit_button)
+
+    def dowlnoad(self):
+        pass
+        # Locate Upload and Download section and the Download button
+
+        upload_download = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.ID, 'item-7')))
+        upload_download.click()
+        download_button = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.ID, 'downloadButton')))
+        download_button.click()
+
+    def close(self):
+        self.driver.quit()
+
+if __name__ == "__main__":
+    webautomation = WebAutomation()
+    webautomation.login("perzak102", "PythonStudent123$")
+    webautomation.fill_form("Piotr Per", "perzak@wp.pl", "Street 1", "Street 2")
+    webautomation.dowlnoad()
+    webautomation.close()
+
+
+
+
+
+
+
+
+
